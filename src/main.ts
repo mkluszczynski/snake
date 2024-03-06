aa99749d3fdebf54845dd572ca0a23a4e822b949
Mateusz Kluszczynski <mkluszczynski@MacBook-Pro-Mateusz.local>
2024-03-06 12:54:38 +0100

    Added terminal rows and columns

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,40 @@
-import {Snake} from "./snake/snake.class";
-import {GameManager} from "./game/gameManager.class";
-import {GameRenderer} from "./game/gameRenderer.class";
-import {sleep} from "./utils/sleep";
-import {InputManager} from "./input/inputManager.class";
-import {SnakeDirectionQueue} from "./snake/snakeDirectionQueue";
-import {FruitManager} from "./fruit/fruitManager.class";
-import {SnakeCollision} from "./snake/snakeCollisionManager";
+import { Snake } from "./snake/snake.class";
+import { GameManager } from "./game/gameManager.class";
+import { GameRenderer } from "./game/gameRenderer.class";
+import { sleep } from "./utils/sleep";
+import { InputManager } from "./input/inputManager.class";
+import { SnakeDirectionQueue } from "./snake/snakeDirectionQueue";
+import { FruitManager } from "./fruit/fruitManager.class";
+import { SnakeCollision } from "./snake/snakeCollisionManager";
 
 const gameManager = new GameManager();
-const snakeDirectionQueue = new SnakeDirectionQueue({x: 1, y: 0})
-const snake = new Snake(gameManager, snakeDirectionQueue, {x: 0, y: 0}, {x: 1, y: 0});
-const fruitManager = new FruitManager(snake)
-const gameRenderer = new GameRenderer(snake, fruitManager, 20, 30)
-const inputManager: InputManager = new InputManager(snakeDirectionQueue, gameRenderer);
-const collisionManager = new SnakeCollision(snake, fruitManager, gameManager)
+const snakeDirectionQueue = new SnakeDirectionQueue({ x: 1, y: 0 });
+const snake = new Snake(
+  gameManager,
+  snakeDirectionQueue,
+  { x: 0, y: 0 },
+  { x: 1, y: 0 },
+);
+const fruitManager = new FruitManager(snake);
+const gameRenderer = new GameRenderer(
+  snake,
+  fruitManager,
+  process.stdout.rows - 1,
+  process.stdout.columns - 1,
+);
+const inputManager: InputManager = new InputManager(
+  snakeDirectionQueue,
+  gameRenderer,
+);
+const collisionManager = new SnakeCollision(snake, fruitManager, gameManager);
 
-async function main(){
-    inputManager.read()
-    while (!gameManager.shouldGameOver()){
-        collisionManager.checkCollision()
-        gameRenderer.render()
-        await sleep(100)
-        snake.move()
-    }
+async function main() {
+  inputManager.read();
+  while (!gameManager.shouldGameOver()) {
+    collisionManager.checkCollision();
+    gameRenderer.render();
+    await sleep(100);
+    snake.move();
+  }
 }
 main();
